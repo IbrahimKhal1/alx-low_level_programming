cc44e3bef71e90c5cda6e9a84e78fe9dcd181f85
Ibrahimkhal1 <halidu.m1602411@st.futminna.edu.ng>
2023-10-20 13:23:36 -0700

    All tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - 4-puts.c
+ * Description: Write a C program that prints a specific string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);
+}
