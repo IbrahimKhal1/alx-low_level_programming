c02b35793fbbe294647aa6d135630fa999c986e4
Ibrahimkhal1 <halidu.m1602411@st.futminna.edu.ng>
2023-10-22 18:18:24 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,19 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 
 int main(void)
 {
-int lowercase = 'a';
-while (lowercase <= 'z')
-{
-putchar (lowercase);
-lowercase += 1;
+char uppercase = 'A'; // Declare the variable at the beginning of the block
+
+while (uppercase <= 'Z') {
+putchar(uppercase);
+uppercase++
 }
+    
 putchar('\n');
+
 return (0);
 }
